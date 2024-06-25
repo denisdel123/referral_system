0e1fb9d64aaa03d5bf2c8c5c0531ebf73a7f1d57
denisbelenko <denisdel123@>
2024-06-25 19:05:20 +0700

    Создал модель пользователя и настроил ее

--- userApp/management/commands/csu.py
+++ userApp/management/commands/csu.py
@@ -1,7 +1,17 @@
+import os
+
 from django.core.management import BaseCommand
+from dotenv import load_dotenv
+
+from userApp.models import User
+
+load_dotenv()
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        ...
+        User.objects.create_superuser(
+            phone_number=os.environ.get("PHONE_SUPERUSER"),
+            password=os.environ.get("PASSWORD_SUPERUSER")
+        )
